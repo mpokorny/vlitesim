313a1dc5143e009198bb6cdadc056e0b539e79bf
Martin Pokorny <mpokorny@nrao.edu>
2014-03-17 16:29:27 -0600

    Add frame rate monitoring
    Add automatic shutdown for clear cases of slow frame generation; also
    remove latency monitoring.

--- src/main/scala/edu/nrao/vlite/Controller.scala
+++ src/main/scala/edu/nrao/vlite/Controller.scala
@@ -1,8 +1,12 @@
 package edu.nrao.vlite
 
 import akka.actor._
+import akka.util.Timeout
+import akka.pattern.ask
 import akka.remote.RemoteScope
 import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.util.{ Success, Failure }
 
 class Controller extends Actor with ActorLogging {
   import context._
@@ -26,24 +30,62 @@ class Controller extends Actor with ActorLogging {
         scope = RemoteScope(settings.remoteAddress(instance.hostname)))),
       s"emulator${index}")
 
-  val emulators: Map[Int, EmulatorInfo] =
-    (settings.emulatorInstances.zipWithIndex map {
-      case (em, index) =>
-        (index, EmulatorInfo(em, emulatorActor(em, index)))
-    }).toMap
+  val emulators: Seq[EmulatorInfo] =
+    settings.emulatorInstances.zipWithIndex map {
+      case (instance, idx) =>
+        EmulatorInfo(instance, emulatorActor(instance, idx))
+    }
 
-  protected def findEmulator(ref: ActorRef): Int = {
-    (emulators find {
-      case (_, EmulatorInfo(_, r)) if r == ref => true
-      case _ => false
-    } map {
-      case (idx, _) => idx
-    }).get
+  implicit val queryTimeout = Timeout(1.second)
+
+  var expectedFrameRates: Option[Vector[Int]] = None
+
+  var recentBufferCounts: Vector[Vector[Long]] = Vector.empty
+
+  case object GetExpectedFrameRates
+  case class ExpectedFrameRates(rates: Vector[Int])
+  case object GetBufferCounts
+  case class BufferCounts(counts: Vector[Long])
+
+  def getExpectedFrameRates {
+    val slf = self
+    Future.traverse(emulators) { em =>
+      (em.actorRef ? Emulator.GetExpectedFrameRate) map {
+        case Emulator.ExpectedFrameRate(rate) => rate
+        case _ => -1
+      }
+    } onComplete {
+      case Success(rates) if rates.forall(_ >= 0) =>
+        slf ! ExpectedFrameRates(rates.toVector)
+      case _ =>
+        slf ! GetExpectedFrameRates
+    }
   }
 
-  protected def foreachEmulator(f: ActorRef => Unit) {
-    emulators foreach {
-      case (_, EmulatorInfo(_, act)) => f(act)
+  def compareToExpected(numSec: Int, start: Vector[Long], end: Vector[Long]) {
+    expectedFrameRates map { expected =>
+      val rateErrors =
+        start zip end map {
+          case (init, fin) => fin - init
+        } zip expected map {
+          case (actual, expected) =>
+            val expectedOverInterval = numSec * expected
+            (actual - expectedOverInterval).toDouble / expectedOverInterval
+        }
+      val slowEmulators = (List.empty[Int] /: rateErrors.zipWithIndex) {
+        case (acc, (err, i)) if err < -0.1 => i :: acc
+        case (acc, _) => acc
+      }
+      if (slowEmulators.length > 0) {
+        log.warning(
+          "Slow frame generation rate from emulator{} {}",
+          if (slowEmulators.length > 1) "s" else "",
+          slowEmulators.reverse.mkString(","))
+        if (rateErrors exists (_ < -0.5)) {
+          log.error("Frame rate too low! Exiting...")
+          system.shutdown()
+        }
+      }
     }
   }
 
@@ -53,25 +95,37 @@ class Controller extends Actor with ActorLogging {
       1.second,
       1.second,
       self,
-      Controller.TriggerDebug)
+      GetBufferCounts
+    )
+    getExpectedFrameRates
   }
 
   def receive: Receive = {
-    case Controller.TriggerDebug =>
-      foreachEmulator { em =>
-        em ! Emulator.GetGeneratorLatencies
-        em ! Transporter.GetBufferCount
+    case GetBufferCounts =>
+      val slf = self
+      Future.traverse(emulators) { em =>
+        (em.actorRef ? Transporter.GetBufferCount) map {
+          case Transporter.BufferCount(count) => count
+          case _ => 0L
+        } recover {
+          case _: Throwable => 0L
+        }
+      } onSuccess {
+        case counts => slf ! BufferCounts(counts.toVector)
       }
-    case latencies: Emulator.Latencies =>
-      log.info("{}", latencies)
-    case count: Transporter.BufferCount =>
-      log.info("emulator{} {}", findEmulator(sender), count)
+    case BufferCounts(counts: Vector[Long]) =>
+      log.info("BufferCounts({})", counts.mkString(","))
+      recentBufferCounts = recentBufferCounts :+ counts
+      if (recentBufferCounts.length >= 6) {
+        recentBufferCounts.takeRight(2) match {
+          case Vector(start, end) => compareToExpected(1, start, end)
+        }
+        recentBufferCounts = recentBufferCounts.tail
+      }
+    case ExpectedFrameRates(rates) =>
+      expectedFrameRates = Some(rates)
+      log.info("ExpectedFrameRates({})", rates.mkString(","))
   }
-}
-
-object Controller {
-  case object TriggerDebug
-  case object GetEmulatorRefs
 }
 
 case class EmulatorInfo(
